8de57d72634a07e8872f12f034bc44751ed33367
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-17 05:50:50 +0530

    Time: 1 ms (99.92%) | Memory: 44.2 MB (75.11%) - LeetSync

---
+++ 581-shortest-unsorted-continuous-subarray/shortest-unsorted-continuous-subarray.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        int n=nums.length,left=-1,right=-2;
+        int max=nums[0],min=nums[n-1];
+        for(int i=1;i<n;i++){
+            max=Math.max(nums[i],max);
+            min=Math.min(nums[n-1-i],min);
+            if(nums[i]<max){
+                right=i;
+            }
+            if(nums[n-1-i]>min){
+                left=n-1-i;
+            }
+        }
+        return right-left+1;
+    }
+}
+/** 
+
+if(nums == null) return 0;
+    if(nums.Length == 0 || nums.Length == 1) return 0;
+    
+    int max = Int32.MinValue;
+    int end = -2;
+    //iterate from beginning of array
+    //find the last element which is smaller than the last seen max from 
+    //its left side and mark it as end
+    for(int i = 0; i < nums.Length; i ++){
+        max = Math.Max(max, nums[i]);
+        if(nums[i] < max)
+            end = i;
+    }
+    
+    int min = Int32.MaxValue;
+    int begin = -1;
+    //iterate from end of array
+    //find the last element which is bigger than the last seen min from 
+    //its right side and mark it as begin
+    for(int i = nums.Length - 1; i >= 0; i --){
+        min = Math.Min(min, nums[i]);
+        if(nums[i] > min)
+            begin = i;
+    }
+    
+    return end - begin + 1;
+
+
+
+ */
