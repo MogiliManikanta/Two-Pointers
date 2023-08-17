d84d7afb091de1874dd327722961274c2b6a1e4c
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-17 19:12:24 +0530

    Time: 14 ms (53.05%) | Memory: 40.1 MB (58.84%) - LeetSync

---
+++ 457-circular-array-loop/circular-array-loop.java
@@ -0,0 +1,46 @@
+
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        int n=nums.length;
+        if(n<=1){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            int slow,fast;
+            slow=i;fast=i;
+            boolean iForward = nums[i]>=0;
+            while(true){
+                slow = getNextIndex(nums,slow,iForward);
+                if(slow==-1){
+                    break;
+                }
+                fast=getNextIndex(nums,fast,iForward);
+                if(fast==-1){
+                    break;
+                }
+                fast=getNextIndex(nums,fast,iForward);
+                if(fast==-1){
+                    break;
+                }
+                if(fast==slow){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private int getNextIndex(int[] nums,int index,boolean iForward){
+        boolean direction = nums[index]>=0;
+        if(iForward!=direction){
+            return -1;
+        }
+        int nextIndex = (index+nums[index])%nums.length;
+        if(nextIndex<0){
+            nextIndex = nextIndex+nums.length;
+        }
+        if(nextIndex==index){
+                return -1;
+        }
+        return nextIndex;        
+    }
+}
