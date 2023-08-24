674e311a6d334886f570240c21ecd1ee3b2d3e32
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-24 07:37:39 +0530

    Time: 26 ms (39.71%) | Memory: 46.2 MB (63.23%) - LeetSync

--- 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
+++ 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
@@ -1,30 +1,28 @@
 class Solution {
-        public String lastSubstring(String s) {
+    public String lastSubstring(String s) {
+        int i=0,j=1,k=0;
         int n = s.length();
-        //k is the len when we have two candidates
-        //i is the first candidate start position, j is the second one (can not be candidate)
-        int i = 0, j= 1, k = 0;
-        while (i < n && j < n && k < n) {
-            if (i + k >= n || j + k >= n) {
+        while(i<n && j<n && k<n){
+            if(i+k >= n || j+k >= n){
                 break;
             }
-            // they have same start point, then increase the length
-            if (s.charAt(i + k) == s.charAt(j + k)) {
+            if(s.charAt(i+k)== s.charAt(j+k)){
                 k++;
-            } else {
-                // now two candidates are different, then which one is larger
-                if (s.charAt(i + k) < s.charAt(j + k)) {
-                    i = i + k + 1; // j becomes the candidate, i need move forward
-                } else {
-                     j = j + k + 1; // i becomes the candidate
+            }
+            else{
+                 if(s.charAt(i+k)<s.charAt(j+k)){
+                    i=i+k+1;
                 }
-                if (i == j) { 
-                    j++; // potational i, j stay at the same position, j move forward(i also can move)
+                else{
+                    j=j+k+1;
                 }
-                k = 0; //reset the len
+                if(i==j){
+                    j++;
+                }
+                k=0;
             }
         }
-        int l = Math.min(i, j);
-        return s.substring(l);
+        int last = Math.min(i,j);
+        return s.substring(last);
     }
 }
