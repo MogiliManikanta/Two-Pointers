93bb43fb513296019deecf43fcadebcad13b3a1b
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-24 19:46:57 +0530

    Time: 4 ms (84.02%) | Memory: 55.6 MB (48.94%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -17,3 +17,13 @@ class Solution {
        return maxArea; 
     }
 }
+/**1.The widest container (using first and last line) is a good candidate, because of its width. Its water level is the height of the smaller one of first and last line.
+
+
+2.All other containers are less wide and thus would need a higher water level in order to hold more water.
+
+
+3.The smaller one of first and last line doesn't support a higher water level and can thus be safely removed from further consideration
+
+1.done
+*/
