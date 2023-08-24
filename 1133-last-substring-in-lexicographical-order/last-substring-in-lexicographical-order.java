315e4d67ea0b21af931ccd401553def5f624f372
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-24 07:38:59 +0530

    Time: 25 ms (44.12%) | Memory: 46.3 MB (63.23%) - LeetSync

--- 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
+++ 1133-last-substring-in-lexicographical-order/last-substring-in-lexicographical-order.java
@@ -1,28 +1,40 @@
 class Solution {
-    public String lastSubstring(String s) {
-        int i=0,j=1,k=0;
+        public String lastSubstring(String s) {
         int n = s.length();
-        while(i<n && j<n && k<n){
-            if(i+k >= n || j+k >= n){
+        //k is the len when we have two candidates
+        //i is the first candidate start position, j is the second one (can not be candidate)
+        int i = 0, j= 1, k = 0;
+        while (i < n && j < n && k < n) {
+            if (i + k >= n || j + k >= n) {
                 break;
             }
-            if(s.charAt(i+k)== s.charAt(j+k)){
+            // they have same start point, then increase the length
+            if (s.charAt(i + k) == s.charAt(j + k)) {
                 k++;
-            }
-            else{
-                 if(s.charAt(i+k)<s.charAt(j+k)){
-                    i=i+k+1;
+            } else {
+                // now two candidates are different, then which one is larger
+                if (s.charAt(i + k) < s.charAt(j + k)) {
+                    i = i + k + 1; // j becomes the candidate, i need move forward
+                } else {
+                     j = j + k + 1; // i becomes the candidate
                 }
-                else{
-                    j=j+k+1;
+                if (i == j) { 
+                    j++; // potational i, j stay at the same position, j move forward(i also can move)
                 }
-                if(i==j){
-                    j++;
-                }
-                k=0;
+                k = 0; //reset the len
             }
         }
-        int last = Math.min(i,j);
-        return s.substring(last);
+        int l = Math.min(i, j);
+        return s.substring(l);
     }
 }
+/**
+
+I think this is o(n) solution
+
+Edit : why it is O(n) and why we can move for i = i + k + 1
+
+bbb…bbbbbba for this case, if we just update i to i + 1,time complexity will downgrade to o(n^2).
+Let me try to prove why it safely move from i to i + k + 1. For example, for "cabcabx" the matching pattern is 'cab', at 'x' two candidates are mismatching. since 'x' > 'c', wen need move i, that's true we can just move to i + 1. However, as long as 'cab' is matching pattern. both 'a' and 'b' have been visited by j. so we can safely move to i + k + 1. In other words, now j becomes i, i becomes to j. why do you want to go back a visited index? the idea is a little similar with KMP.
+ 
+ */
